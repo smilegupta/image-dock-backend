f01c10319bed4c2e06d09a96916e0553e45160d7
smilegupta <guptamiley3012@gmail.com>
2021-04-14 00:52:38 +0530

    fixed issue

--- ImageDock/index.js
+++ ImageDock/index.js
@@ -4,22 +4,23 @@ const uuid = require('uuid');
 exports.handler = async (event, context) => {
     console.log(event);
     const collectionId = uuid.v4();
+    const input = JSON.parse(event.body);
     console.log({TableName: process.env.USER_IMAGES_TABLENAME,
         Item: {
-            userId: event.body.userId,
+            userId: input.userId,
             collectionId: collectionId,
             createdAt: new Date().toISOString(),
-            collectionName: event.body.collectionName,
-            collectionDescription: event.body.collectionDescription
+            collectionName: input.collectionName,
+            collectionDescription: input.collectionDescription
         }})
     await putItem({
         TableName: process.env.USER_IMAGES_TABLENAME,
         Item: {
-            userId: event.body.userId,
+            userId: input.userId,
             collectionId: collectionId,
             createdAt: new Date().toISOString(),
-            collectionName: event.body.collectionName,
-            collectionDescription: event.body.collectionDescription
+            collectionName: input.collectionName,
+            collectionDescription: input.collectionDescription
         }
     });
     return {
