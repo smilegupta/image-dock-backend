21e882c914f3c2f3536cb16424c9ed7a0ad72c75
smilegupta <guptamiley3012@gmail.com>
2021-04-14 00:45:14 +0530

    collection function

--- ImageDock/index.js
+++ ImageDock/index.js
@@ -1,7 +1,27 @@
 const {putItem} = require("./db-helpers");
+const uuid = require('uuid');
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
     console.log(event);
+    const collectionId = uuid.v4();
+    console.log({TableName: process.env.USER_IMAGES_TABLENAME,
+        Item: {
+            userId: event.body.userId,
+            collectionId: collectionId,
+            createdAt: new Date().toISOString(),
+            collectionName: event.body.collectionName,
+            collectionDescription: event.body.collectionDescription
+        }})
+    await putItem({
+        TableName: process.env.USER_IMAGES_TABLENAME,
+        Item: {
+            userId: event.body.userId,
+            collectionId: collectionId,
+            createdAt: new Date().toISOString(),
+            collectionName: event.body.collectionName,
+            collectionDescription: event.body.collectionDescription
+        }
+    });
     return {
         statusCode : 200,
         body: "Collection Created Successfully"
