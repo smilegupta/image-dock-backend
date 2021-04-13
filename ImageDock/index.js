005bbaf383927f3cabfbf2cd8f4fe1b169fbc7e8
smilegupta <guptamiley3012@gmail.com>
2021-04-14 02:15:24 +0530

    2 more api added

--- ImageDock/index.js
+++ ImageDock/index.js
@@ -1,10 +1,9 @@
-const {putItem} = require("./db-helpers");
+const {putItem,queryItems} = require("./db-helpers");
+const {getInputParams} = require("./utils");
 const uuid = require('uuid');
 
-exports.handler = async (event, context) => {
-    console.log(event);
+async function createCollection (input){
     const collectionId = uuid.v4();
-    const input = JSON.parse(event.body);
     console.log({TableName: process.env.USER_IMAGES_TABLENAME,
         Item: {
             userId: input.userId,
@@ -25,6 +24,71 @@ exports.handler = async (event, context) => {
     });
     return {
         statusCode : 200,
-        body: "Collection Created Successfully"
+        body: {collectionId}
     }
 }
+async function getCollection(userId, collectionId){
+    const res = await queryItems({
+        TableName: process.env.USER_IMAGES_TABLENAME,
+        KeyConditionExpression: `userId=:userId and collectionId=:collectionId`, 
+        IndexName: "userId-collectionId-index",
+        ExpressionAttributeValues: {":userId": userId, ":collectionId": collectionId}
+    });
+    return{
+        statusCode: 200,
+        body: res.Items
+    }
+}
+
+async function getCollectionsForUser(userId){
+    const res = await queryItems({
+        TableName: process.env.USER_IMAGES_TABLENAME,
+        KeyConditionExpression: `userId=:userId`, 
+        ExpressionAttributeValues: {":userId": userId}
+    });
+    return{
+        statusCode: 200,
+        body: res.Items
+    }
+}
+
+async function addingImagetoCollection (input){
+    await putItem({
+        TableName: process.env.USER_IMAGES_TABLENAME,
+        Item: {
+            userId: input.userId,
+            collectionId: input.collectionId,
+            createdAt: new Date().toISOString(),
+            collectionName: input.collectionName,
+            collectionDescription: input.collectionDescription,
+            imageUrl: input.imageUrl
+        }
+    });
+    return {
+        statusCode : 200,
+        body: "Added Successfully"
+    }
+}
+
+exports.handler = async (event, context) => {
+    console.log(event);
+    const {resource, body, httpMethod, queryParams, pathParams } = getInputParams(event);
+    if (httpMethod === "POST" && resource === "/collection"){
+        return createCollection(body);
+    }
+    if (httpMethod === "GET" && resource === "/collection/{collectionId}"){
+        const userId = queryParams.userId;
+        const collectionId = pathParams.collectionId;
+        console.log("query", queryParams)
+        return getCollection(userId, collectionId);
+    }
+    if (httpMethod === "GET" && resource === "/collection"){
+        const userId = queryParams.userId;
+        return getCollectionsForUser(userId);
+    }
+    if (httpMethod === "POST" && resource === "/collection/image"){
+        return addingImagetoCollection(body);
+    }
+    
+}
+
